ad2a6145aa2c240507eda637dd4aeb83b4cbdaa4
ilimKUSH <ikusbekov@gmail.com>
2025-03-11 16:49:20 +0600

    fix: build

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ import '@/styles/global.css';
 import {I18nProvider} from '@/components/core/i18n-provider';
 import {LocalizationProvider} from '@/components/core/localization-provider';
 import {ThemeProvider} from '@/components/core/theme-provider/theme-provider';
+import {GlobalStyles, Box} from "@mui/material";
 // import { Toaster } from '@/components/core/toaster';
 
 export const viewport = {
@@ -21,7 +22,47 @@ export default async function RootLayout({children}) {
     <LocalizationProvider>
       <I18nProvider language="en">
         <ThemeProvider>
-          {children}
+          <React.Fragment>
+            <GlobalStyles
+              styles={{
+                body: {
+                  '--MainNav-height': '56px',
+                  '--MainNav-zIndex': 1000,
+                  '--SideNav-width': '280px',
+                  '--SideNav-zIndex': 1100,
+                  '--MobileNav-width': '320px',
+                  '--MobileNav-zIndex': 1100,
+                },
+              }}
+            />
+            <Box
+              sx={{
+                bgcolor: 'var(--mui-palette-background-default)',
+                display: 'flex',
+                flexDirection: 'column',
+                position: 'relative',
+                minHeight: '100%',
+              }}
+            >
+              <Box sx={{display: 'flex', flex: '1 1 auto', flexDirection: 'column'}}>
+                <Box
+                  component="main"
+                  sx={{
+                    '--Content-margin': '0 auto',
+                    '--Content-paddingX': '24px',
+                    '--Content-paddingY': {xs: '24px', lg: '64px'},
+                    '--Content-padding': 'var(--Content-paddingY) var(--Content-paddingX)',
+                    '--Content-width': '100%',
+                    display: 'flex',
+                    flex: '1 1 auto',
+                    flexDirection: 'column',
+                  }}
+                >
+                  {children}
+                </Box>
+              </Box>
+            </Box>
+          </React.Fragment>
           {/*<Toaster position="bottom-right" />*/}
         </ThemeProvider>
       </I18nProvider>
