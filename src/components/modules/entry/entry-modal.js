295f3155526b18362e2d60fdccd70cf3a2e16b57
Almaz <red.rokku@gmail.com>
2025-05-29 17:27:37 +0600

    changed onClick event

--- src/components/modules/entry/entry-modal.js
+++ src/components/modules/entry/entry-modal.js
@@ -46,27 +46,33 @@ export default function EntryModal() {
 
   return (
     <>
-      <Button variant="contained" color="secondary" onClick={handleOpen}>
+      <Button
+        variant="contained"
+        color="secondary"
+        onClick={() => {
+          window.location.href = process.env.NEXT_PUBLIC_API_DOMEN;
+        }}
+      >
         Войти
       </Button>
 
-      <Dialog open={open} onClose={handleClose} fullWidth maxWidth="sm">
-        <Stack direction="row" p={2} sx={{ paddingBottom: 0 }}>
-          <Stack direction="row" spacing={2} sx={{ flex: "1 1 auto", alignItems: "center" }}>
-            <Image src="/logo.png" alt="logo" width={50} height={50} style={{ borderRadius: "50%", backgroundColor: "white", padding: 5 }} />
-            <Typography variant="h6" fontWeight={700}>
-              ГипроЗем ГИС системы
-            </Typography>
-          </Stack>
-          <IconButton onClick={handleClose} size="small">
-            <CloseIcon />
-          </IconButton>
-        </Stack>
+      {/*<Dialog open={open} onClose={handleClose} fullWidth maxWidth="sm">*/}
+      {/*  <Stack direction="row" p={2} sx={{ paddingBottom: 0 }}>*/}
+      {/*    <Stack direction="row" spacing={2} sx={{ flex: "1 1 auto", alignItems: "center" }}>*/}
+      {/*      <Image src="/logo.png" alt="logo" width={50} height={50} style={{ borderRadius: "50%", backgroundColor: "white", padding: 5 }} />*/}
+      {/*      <Typography variant="h6" fontWeight={700}>*/}
+      {/*        ГипроЗем ГИС системы*/}
+      {/*      </Typography>*/}
+      {/*    </Stack>*/}
+      {/*    <IconButton onClick={handleClose} size="small">*/}
+      {/*      <CloseIcon />*/}
+      {/*    </IconButton>*/}
+      {/*  </Stack>*/}
 
-        <DialogContent>
-          <UsernameLoginForm />
-        </DialogContent>
-      </Dialog>
+      {/*  <DialogContent>*/}
+      {/*    <UsernameLoginForm />*/}
+      {/*  </DialogContent>*/}
+      {/*</Dialog>*/}
     </>
   );
 }
